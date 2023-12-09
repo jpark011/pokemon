5565d8e6eaacf7d8527fe3436c94067f19aadc45
jay.now(박현재)/kakao <jay.now@kakaocorp.com>
2023-12-09 20:31:26 +0900

    ➕ heroicons

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { PlusIcon } from "@heroicons/react/20/solid";
+import { useState } from "react";
+import "./App.css";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -18,7 +19,11 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button
+          className="flex gap-2 align-middle leading-normal"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          <PlusIcon className="h-5 w-5" />
           count is {count}
         </button>
         <p>
@@ -29,7 +34,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
